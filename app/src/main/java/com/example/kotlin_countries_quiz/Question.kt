474de01e1c56b69624a52ea1bdddc6d0422cdbc3
BinaryLeo <leomouracss@fastmail.com>
2023-03-31 07:24:54 -0300

    FIX: implementation ktx

---
+++ app/src/main/java/com/example/kotlin_countries_quiz/Question.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin_countries_quiz
+
+data class Question(
+    val id: Int,
+    val questions: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAnswer: Int
+)
